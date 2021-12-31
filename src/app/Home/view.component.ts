f44afa360594a952e40d9bb8fe18ad420c650e19
arip995 <arindampanda2002@gmail.com>
2021-12-31 09:38:43 +0530

    added home component styling

---
+++ src/app/Home/view.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'view-home',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss'],
+})
+export class HomeComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+  }
+}
