6f165f3e67017dff21a7b5f157a5b9ae30638688
arip995 <arindampanda2002@gmail.com>
2022-01-26 18:06:00 +0530

    prod setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
+  adminBasePath:"http://127.0.0.1:8000/worker",
+  customerBasePath:"http://127.0.0.1:8000/worker",
+  workerBasePath:"http://127.0.0.1:8000/worker",
   production: true
 };
