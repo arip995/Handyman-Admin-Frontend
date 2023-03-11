c2a28675d79f3a03cbf7c6393b57b427a58ee105
arip997 <arindam.panda@zineone.com>
2023-03-11 12:38:30 +0530

    fixed signup problem

--- src/app/Authentication/sign-in/sign-in.component.ts
+++ src/app/Authentication/sign-in/sign-in.component.ts
@@ -1,59 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'sign-up',
   templateUrl: './sign-in.component.html'
 })
 export class SignInComponent implements OnInit {
-    signInForm: FormGroup;
-    myDate:any = new Date();
-    //Constructor
-    constructor(
-      private _formBuilder: FormBuilder,
-      private _httpClient: HttpClient,
-      private _activeRoute: ActivatedRoute,
-      private _router: Router,
-      private _datePipe: DatePipe
-      ){
-        this.signInForm = _formBuilder.group({
-            username : new FormControl("",[Validators.required,Validators.minLength(6),Validators.maxLength(10)]),
-            password : new FormControl("",[Validators.required]),
-          });
+  signInForm: FormGroup;
+  myDate: any = new Date();
+  //Constructor
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _httpClient: HttpClient,
+    private _activeRoute: ActivatedRoute,
+    private _router: Router,
+    private _datePipe: DatePipe,
+    private _snackBar: MatSnackBar,
+  ) {
+    this.signInForm = _formBuilder.group({
+      username: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(10)]),
+      password: new FormControl("", [Validators.required]),
+    });
+  }
+
+  //OnInit method
+  ngOnInit() {
+  }
+
+  checkError(errorName: any) {
+
+    return () => {
+      this.signInForm.controls['username'].hasError(errorName);
+      this.signInForm.controls['password'].hasError(errorName);
     }
 
-    //OnInit method
-    ngOnInit() {
+  }
+
+  signIn() {
+    const data = {
+      "username": this.signInForm.get('username')?.value,
+      "password": this.signInForm.get('password')?.value
     }
 
-    checkError(errorName:any){
-  
-        return()=>{
-          this.signInForm.controls['username'].hasError(errorName);
-          this.signInForm.controls['password'].hasError(errorName);
-        } 
-    
-      }
-
-    signIn(){
-      const data = {
-        "username" : this.signInForm.get('username')?.value,
-        "password" : this.signInForm.get('password')?.value
-      }
-
-      this._httpClient.post(`http://127.0.0.1:8000/handymanadmin/signin/`,data).subscribe
-      ((res:any)=>{
+    this._httpClient.post(`http://127.0.0.1:8000/handymanadmin/signin/`, data).subscribe
+      ((res: any) => {
         console.log(res);
-        const localitem:any = localStorage.getItem('adminAccessToken');
-        if(localitem ){
-          
-        }else{
+        const localitem: any = localStorage.getItem('adminAccessToken');
+        if (res.status == false) {
+          this._snackBar.open("Your Account is not activated, please try after sometime", "OK", {
+            duration: 8000
+          });
+          return;
+        } else {
           localStorage.setItem('adminAccessToken', res.accessToken);
+          this._router.navigate(['../home'], { relativeTo: this._activeRoute });
         }
-        this._router.navigate(['../home'],{relativeTo : this._activeRoute});
       })
-    }
+  }
 }
